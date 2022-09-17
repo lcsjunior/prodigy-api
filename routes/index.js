be832d457f426e98ac496f009fc3cc520eecbc85
Luiz Carlos Jr <lcsjuniorsi@gmail.com>
2022-09-16 22:44:37 -0300

    fix: fix duplicated import

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const { index } = require('../controllers/index');
 const { login, logout, detailUser } = require('../controllers/auth');
-const { isAuthenticated } = require('../config/passport');
+const { passport, isAuthenticated } = require('../config/passport');
 
 router.get('/', index);
 router.get('/user', isAuthenticated, detailUser);
