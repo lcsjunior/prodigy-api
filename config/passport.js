4a3723cc2b8fafc9c41b14d8d33208aef1507232
Luiz Carlos Jr <lcsjuniorsi@gmail.com>
2022-10-13 22:50:06 -0300

    feat: add a webview

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
 const { Strategy: LocalStrategy } = require('passport-local');
 const { Op } = require('sequelize');
 const { User, Role, Sequelize } = require('../models');
@@ -25,20 +26,22 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    const user = await User.findOne({
-      where: { [Op.or]: { username, email: username } },
-    });
-    if (user) {
-      const match = await user.isValidPassword(password);
-      if (match) {
-        return done(null, user);
-      }
+const verify = async (username, password, done) => {
+  const user = await User.findOne({
+    where: { [Op.or]: { username, email: username } },
+  });
+  if (user) {
+    const match = await user.isValidPassword(password);
+    if (match) {
+      return done(null, user);
     }
-    return done(null, false);
-  })
-);
+  }
+  return done(null, false);
+};
+
+passport.use(new LocalStrategy(verify));
+
+passport.use(new BasicStrategy(verify));
 
 const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const { index } = require('../controllers/index');
 const { login, logout, detailUser } = require('../controllers/auth');
 const { passport, isAuthenticated } = require('../config/passport');
+const { isDev } = require('../utils/current-env');
 
 router.get('/', index);
 router.get('/user', isAuthenticated, detailUser);
+router.get('/login', passport.authenticate('basic', { session: isDev }), login);
 router.post('/login', passport.authenticate('local'), login);
-router.post('/logout', logout);
+router.all('/logout', logout);
 
 module.exports = router;
